9f2350255d0c5cbaa574c88032d65550e4849950
dgabets <dgabets@gmail.com>
2018-11-24 17:04:01 +0300

    Added count episodes

--- clutch-json-api.php
+++ clutch-json-api.php
@@ -14,6 +14,17 @@ function json_api_import_clutch_episode_wp_post($this_obj, $wp_post) {
         $this_obj->set_value('messages', $parser->resultMessages);
         $this_obj->set_value('parser_warnings', $parser->warnings);
     }
+
+    if ($wp_post->post_type === "clutch_story")
+    {
+        $pages = get_pages( array( 'child_of' => $wp_post->ID, 'post_type' => 'clutch_episode'));
+        $count_clutch_episode = 0;
+        if (!empty($pages))
+        {
+            $count_clutch_episode = count($pages);
+        }
+        $this_obj->set_value('count_clutch_episode', $count_clutch_episode);
+    }
 }
 
 add_action( 'json_api_import_wp_post', 'json_api_import_clutch_episode_wp_post', 10, 2 );
